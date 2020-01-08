81cb597d28b01c1d092687b5050986af5fa2ebf9
jaradat-pdb <admin@pdbcorp.org>
2020-01-09 00:54:37 +0200

    Added function to add a user inputted ToDo to the list, addressed a bug where the click-to-remove function would not work on newly added li elements by placing the listener on the ul element and adding a second parameter to specify when to run the listener function, readded the crossing out function and setup code so that a ToDo task is removed from the list when the user clicks on the span element and crosses/checks a ToDo when anywhere else in the li is clicked (mod-226)

--- apps/resources/js/sec20_todo-list.js
+++ apps/resources/js/sec20_todo-list.js
@@ -1,12 +1,25 @@
 if($("body")) {
     console.log("HALLO!");
 }
-$("li>span").click(function(event) {
+
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "li>span", function(e) {
     // will fade out the parent element over a period of 1s then call the function to have it removed
     $(this).parent().fadeOut(1000, function() {
         // this refers to the parent and not the child from the line above
         $(this).remove();
     });
     // ref: https://www.tutorialspoint.com/jquery/event-stoppropagation.htm
-    event.stopPropagation();
-})
+    e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e) {
+    if(e.which === 13) {
+        $("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+        console.log("Added new ToDo: " + $(this).val());
+        $(this).val("");
+    }
+});
