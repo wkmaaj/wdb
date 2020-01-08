6bc4725da6db540e8d9b6560f601723465d18e22
jaradat-pdb <admin@pdbcorp.org>
2020-01-08 23:42:57 +0200

    Setup todo list to fade an element out from the html page and then remove it from the list when clicked (mod-224)

--- apps/resources/js/sec20_todo-list.js
+++ apps/resources/js/sec20_todo-list.js
@@ -1,7 +1,12 @@
 if($("body")) {
     console.log("HALLO!");
 }
-$("li>span").click(function() {
-    // $(this).parent().css("display", "none");
-    $(this).parent().toggleClass("completed");
+$("li>span").click(function(event) {
+    // will fade out the parent element over a period of 1s then call the function to have it removed
+    $(this).parent().fadeOut(1000, function() {
+        // this refers to the parent and not the child from the line above
+        $(this).remove();
+    });
+    // ref: https://www.tutorialspoint.com/jquery/event-stoppropagation.htm
+    event.stopPropagation();
 })
