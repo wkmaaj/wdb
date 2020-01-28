c817a7dfec03f7a1019dc9b1132ecf5ad9c4593a
jaradat-pdb <jaradat.pdb@gmail.com>
2020-01-28 09:45:15 +0200

    Added error handling logic to deal with non-200 responses (such as 401), consolidated error handling into a single function, enhanced error logging, and other minor clean-up refactors

--- apps/movie-search-app/app.js
+++ apps/movie-search-app/app.js
@@ -12,25 +12,26 @@ app.get("/", function(req, res) {
 app.get("/results", function(req, res) {
 	let url = "http://omdbapi.com?s=" + req.query.search + "&apikey=thewdb";
 	request(url, function(error, response, body) {
-		if(error) {
-			console.error(error);
+		if(error || (response.statusCode !== 200)) {
+			errorHandler(res, req.query.search, (error ? error : (`Received invalid ${response.statusCode} response`)));
 		}
 		else {
-			console.log("statusCode: ", response && response.statusCode);
-			if(response.statusCode === 200) {
-				let results = JSON.parse(body);
-				if(results["Error"]) {
-					console.error(results["Error"]);
-					res.render("error", {search: req.query.search});
-				}
-				else {
-					console.log(`totalResults: ${results.totalResults}`);
-					res.render("results", {search: req.query.search, results: results});
-				}
+			let results = JSON.parse(body);
+			if(results["Error"]) {
+				errorHandler(res, req.query.search, `Search for '${req.query.search}' returned '${results["Error"]}'`);
+			}
+			else {
+				console.log(`totalResults: ${results.totalResults}`);
+				res.render("results", {search: req.query.search, results: results});
 			}
 		}
 	});
 });
+
+function errorHandler(res, search, msg) {
+	console.error(msg);
+	res.render("error", {search: search});
+}
 
 app.listen(3000, function() {
 	console.log("Server up, running, listening, and of course is configured to port 3000");
