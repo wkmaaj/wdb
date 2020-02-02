761f3ab37788ec79150ee30848e88307bf331aa9
jaradat-pdb <jaradat.pdb@gmail.com>
2020-02-02 17:04:03 +0200

    Added a page to add new campground info to site along with body-parser dependency to extract the new campground info from a POST request

--- apps/yelp-camp/app.js
+++ apps/yelp-camp/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const campgrounds = [
 	{name: "Salmon Creek", image: "https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c7d2f7add9148c551_340.jpg"},
@@ -6,6 +7,7 @@ const campgrounds = [
 	{name: "Mountain Goat's Rest", image: "https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c7d2f7add9148c551_340.jpg"}
 ];
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("static"));
 app.use(express.static("../resources/css/lib"));
 app.use(express.static("../resources/js/lib"));
@@ -15,8 +17,19 @@ app.get("/:var((home|index)(.html|.ejs)?)?", (req, res) => {
 	res.render("index");
 });
 
-app.get("/campgrounds", function(req, res) {
+app.get("/campgrounds", (req, res) => {
 	res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", (req, res) => {
+	req.body.name;
+	req.body.image;
+	campgrounds.push({name: req.body.name, image: req.body.image});
+	res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", (req, res) => {
+	res.render("new");
 });
 
 app.listen(3000, function() {
