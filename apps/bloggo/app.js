09cac73f18959d9b1e11032ffb5ec39460ea5128
jaradat-pdb <jaradat.pdb@gmail.com>
2020-02-09 15:15:25 +0200

    Initial commit of application code logic

---
+++ apps/bloggo/app.js
@@ -0,0 +1,76 @@
+const bodyParser = require("body-parser"),
+	express = require("express"),
+	mongoose = require("mongoose"),
+	app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("../resources/css/lib"));
+app.use(express.static("../resources/js/lib"));
+app.use(express.static("static"));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/bloggo", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+const Bloggo = mongoose.model("Blog", new mongoose.Schema({
+	title: String,
+	image: {
+		type: String,
+		default: "https://images.unsplash.com/photo-1490029664681-78a95e3d126d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1084&q=80"
+	},
+	body: String,
+	created: {
+		type: Date,
+		default: Date.now
+	}
+}));
+
+app.get("/:var((blogs|home|index)(.html|.ejs)?)?", (req, res) => {
+	Bloggo.find({}, (err, blogs) => {
+		if(err || (blogs.length === 0)) {
+			console.error("Unsuccessful query operation\n" + ((err) ? err : blogs.length));
+			res.redirect("/blogs/new");
+		}
+		else {
+			console.log("Successful query operation, " + blogs.length + " blog posts returned");
+			res.render("index", {blogs: blogs});
+		}
+	});
+});
+
+app.get("/blogs/new", (req, res) => {
+	res.render("new");
+});
+
+app.post("/blogs", (req, res) => {
+	Bloggo.create(req.body.blog, function(err, blog) {
+		if(err) {
+			console.error("Unsuccessful save operation\n" + err);
+			res.render("new");
+		}
+		else {
+			console.log("Successful save operation\n" + blog);
+			res.redirect("/blogs");
+		}
+	});
+});
+
+app.get("/blogs/:id", (req, res) => {
+	Bloggo.findById(req.params.id, (err, blog) => {
+		if(err) {
+			console.error("Unsuccessful query operation\n" + err);
+			res.redirect("/blogs");
+		}
+		else {
+			console.log("Successful query operation, returning " + blog.title + " blog post");
+			res.render("show", {blog: blog});
+		}
+	});
+});
+
+app.listen(3000, function() {
+	console.log("blogGO is RESTed, up, and running on port 3000");
+})
