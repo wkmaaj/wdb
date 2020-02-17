b9a3c80a0605ffd3f7b6ab72987e5166f0b7c0c8
jaradat-pdb <jaradat.pdb@gmail.com>
2020-02-17 23:31:18 +0200

    Add price field to campground model

--- apps/ycamp/models/campground.js
+++ apps/ycamp/models/campground.js
@@ -4,6 +4,10 @@ const campgroundSchema = new mongoose.Schema({
 	name: String,
 	image: String,
 	desc: String,
+	price: {
+		currency: String,
+		value: String
+	},
 	author: {
 		id: {
 			type: mongoose.Schema.Types.ObjectId,
